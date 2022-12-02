27646594a3dde358b3211f0af69fb6f96873b9af
郭佳欣 <acmenxin@163.com>
2022-12-02 22:01:16 +0800

    feat: logo与字体

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import styles from "./App.module.css"
 import robots from "./mockdate/robot.json"
 import Robot from './components/Robots';
+import logo from "./assets/images/logo.svg"
+import ShoppingCart from "./components/ShoppingCard"
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+    <img src={logo} alt="logo" className={styles.appLogo} />
+    <h1 >电商平台</h1>
+      </div>
+      <ShoppingCart />
       <ul className={styles.robotList}>
         {robots.map((item) => {
           return (
