79ce3df418fcdebbe91eaf9eb48aea6d81268481
郭佳欣 <acmenxin@163.com>
2022-12-03 00:01:40 +0800

    feat: 捕获error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./App.module.css"
 import Robot from './components/Robots';
 import logo from "./assets/images/logo.svg"
@@ -10,37 +10,45 @@ interface Props {
 interface State {
   robotGallery: any[]
 }
-
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robotGallery: []
+const App: React.FC = (props) => {
+  const [robotGallery, setRobotGallery] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>()
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const responses = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await responses.json();
+        setRobotGallery(data);
+      } catch (e:any) {
+        setError(e.message);
+      }
+      setLoading(false);
     };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
-  render(): React.ReactNode {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} alt="logo" className={styles.appLogo} />
-          <h1 >电商平台</h1>
-        </div>
-        <ShoppingCart />
+    fetchData();
+  }, []);
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1 >电商平台</h1>
+      </div>
+      <ShoppingCart />
+      {(!error || error !== "") && <div>网站出错：{error}</div>}
+      {!loading ? (
         <ul className={styles.robotList}>
-          {this.state.robotGallery.map((item) => {
+          {robotGallery.map((item) => {
             return (
               <Robot id={item.id} name={item.name} email={item.email} />
             )
           })}
-        </ul>
-      </div>
-    );
-  }
+        </ul>) 
+        :(<h2>loading 加载中</h2>)
+      }
+    </div>
+  )
 }
 export default App;
